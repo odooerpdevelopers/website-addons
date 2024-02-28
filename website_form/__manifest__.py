d1806a0152e8ee462b53251a98c07554dedc950e
odooerpdevelopers <odooerpdevelopers@gmail.com>
2024-02-28 12:26:09 +0100

    Add website page and website form

---
+++ website_form/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Widget Form',
+    'version': '1.0',
+    'summary': 'JS Widgets, Forms for Odoo Website',
+    'description': """
+    Widgets in Odoo
+    This app enables you to bind JavaScript widgets to forms on websites from scratch.
+    """,
+    'category': 'Custom',
+    'author': 'odooexperto.com',
+    'website': 'https://odooexperto.com',
+    'license': 'AGPL-3',
+    'depends': ['base', 'portal',  'web', 'website'],
+    'data': [
+        'views/templates.xml'
+    ],
+
+    'installable': True,
+    'auto_install': False,
+}
